28a6f71c18d0886c72495fd862aaba6d1217e3ca
renuk <renuk@HP-RL>
2025-12-03 15:15:19 +0530

    First commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,16 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+	@Test
+	public void githubPracticeTest() {
+		Reporter.log("Hii Github", true);
+		//String BROWSER = System.getProperty("browser");
+		//System.out.println(BROWSER);
+		//String USERNAME = System.getProperty("username");
+		//System.out.println(USERNAME);
+	}
+
+}
